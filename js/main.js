fff29a77045aa0a3440ca74ab3037a501224d759
Rohan Tayal <109153434+rohantayal@users.noreply.github.com>
2024-05-29 22:33:14 +0530

    implemented active state

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded",()=> {
     const share=document.getElementById("share-icon");
-    
+
+    share.addEventListener('click',()=>{
+        const footers=document.querySelectorAll(".sub-left");
+        const parent=document.querySelector('footer');
+        console.log(parent);
+        footers.forEach(footer => {
+            if(footer.classList.contains('active')){
+                footer.classList.remove('active');
+                parent.classList.remove('background_change');
+            }
+            else{
+                footer.classList.add('active');
+                parent.classList.add('background_change');
+            }
+            
+        });
+    })
 });
